e41ade229bdc1814f83a825b592f0e624b285fd2
Perez Gavilan <Carla.PerezGavilan@Bain.com>
2022-06-02 11:00:46 -0500

    first draft of code strucutre

---
+++ main.exs
@@ -0,0 +1,55 @@
+
+# Language Parser
+# 
+# Carla Perez Gavilan Del Castillo
+# Gerardo Angeles 
+# David Medina
+
+defmodule Parser do 
+@moduledoc """
+  A module that recieves a python file and returns and html file 
+  """
+
+   @doc """
+  Recieves name of file returns lines of file in list form
+  """
+  def read_file(file_name) do
+ 
+    end 
+
+     @doc """
+    Write output back to file 
+  """
+  def read_file(file_name) do
+ 
+    end 
+
+   @doc """
+    Returns lines in html format 
+  """
+    def format_html(output) do
+
+    end 
+
+ @doc """
+    Key word identifier: recieves line identifies keywords and gives them a different color in output file 
+  """
+    def keyword_identify(line) do
+
+    end
+
+    @doc """
+    Operator identifier: recieves a line identifies operators and gives them a different color in output file 
+  """
+    def  operator_identify(line) do
+   
+    end
+
+    
+    @doc """
+    Variable identifier: recieves a line identifies variables and gives them a different color in output file 
+  """
+    def  variable(line) do
+   
+    end
+end
