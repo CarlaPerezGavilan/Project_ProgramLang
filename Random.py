6322146afb04bafa4a065738eff33745b17627ed
Gerardo Ángeles Hernández <gerardo788@live.com>
2022-06-07 02:34:41 -0500

    Varias funcionalidades creadas
    funcion para agregar informacion de HTML por clase
    ya se lee archivo
    ya se encuentran los comentarios
    ya se encuentran las palabras restringidas de python
    ya se identificaron las lineas que no son comentarios
    se escribe archivo

--- Random.py
+++ Random.py
@@ -1,12 +1,7 @@
 # Program to generate a random number between 0 and 9
 
 # importing the random module
-from ast import main
 import random
 
-print(random.randint(0,9))
-
-def myfunction():
-    print("hola")
-
-myfunction()
+if 2 > 1 and 2 > 1:
+    print(random.randint(0,9))
--- main.exs
+++ main.exs
@@ -1,65 +1,157 @@
 
-# Language Parser
-# 
-# Carla Perez Gavilan 
-# Gerardo Angeles 
+# Language Python Parser
+#
+# Carla Perez Gavilan
+# Gerardo Angeles A01338190
 # David Medina
 
-defmodule Parser do 
+# To run the program
+# Parser.main("Random.py", "prueba.html")
+
+defmodule Parser do
+
 @moduledoc """
-  A module that recieves a python file and returns and html file 
+  A model that implements functions for performing a parser for
+  the pyhton programming language
   """
 
-    @doc """
-    Recieves name of file returns lines in file in list form
-    """
-    def read_file(file_name) do
-    
-    end 
-
-     @doc """
-    Process each line
-        """
-    def process_line(line) do
-        # Call identify variables
-        # Call identify operators
-        # Call identify keywords
-        # Format line html
-    end
-
-     @doc """
-    Write output back to file 
-    """
-    def read_file(file_name) do
- 
-    end 
-
-   @doc """
-    Returns line in html format 
+  @doc """
+  A module that recieves a python file and returns and html file
   """
-    def format_html(line) do
+  def main(in_filename, out_filename) do
 
-    end 
+    new_content =
+      in_filename
+      #real file
+      |> read_file()
+      # all the line process
+      |> process_line()
+      #join the lines with new lines
+      |> Enum.join("\n")
+      # add the HTML body document
+      |> add_HTML_base_information(out_filename)
 
-    @doc """
-    Key word identifier: recieves line identifies keywords and gives them a different color in output file 
-    """
-    def keyword_identify(line) do
+    write_file(out_filename, new_content)
 
+  end
+
+
+  @doc """
+  Recieves name of file returns lines in file in list form
+  """
+  def read_file(file_name) do
+    file_name
+    |> File.stream!()
+    |> Enum.map(&String.trim/1)
+  end
+
+  @doc """
+  Process each line
+  """
+  def process_line(line) do
+    new_content =
+      line
+      #search for the symbol # to decide that line is a comment
+      |> Enum.map(&comment_search(&1))
+      #if no comment line, search fot keywords, variables, operators, functions,etc
+      |> Enum.map(&not_comment_identifier(&1))
+      #add the html information to a line
+      |> Enum.map(&format_html(&1))
+  end
+
+  @doc """
+  to analize the lines that arent comments
+  """
+  def not_comment_identifier(line) do
+    #no comment line
+    if Regex.run(~r/^((?!#).)*$/, line) do
+      #split line
+      String.split(line, " ")
+      # search for keywords
+      |> Enum.map(&keyword_identify(&1))
+      #HERE ADD THE OTHER FUNCTIONS FOR SEARCH VARIABLES, NUMBERS, FUNCTIONS, ETC.
+      |> Enum.join(" ")
+    else
+      #if line return normal line
+      line
     end
+  end
 
-    @doc """
-    Operator identifier (=, +, -, *): recieves a line identifies operators and gives them a different color in output file 
-    """
-    def  operator_identify(line) do
-   
-    end
+  @doc """
+  Search for comments and add the corresponding tag
+  """
+  def comment_search(to_analize) do
+    Regex.replace(~r/(#(.*))/, to_analize, &add_information(&1, "comment") , global: false)
+  end
 
-    
-    @doc """
-    Variable identifier: recieves a line identifies variables and gives them a different color in output file 
-    """
-    def variable_identify(line) do
-   
-    end
+  @doc """
+  Add the HTML information to the document
+  """
+  def add_HTML_base_information(body, out_filename) do
+    content =
+      "<!doctype html>
+      <html lang='en'>
+          <head>
+              <meta charset='utf-8'>
+              <title>#{out_filename}</title>
+              <style>
+                  .comment {color: gray;}
+                  .keyword {color: blue;}
+                  .parenthesis {color: rgb(255, 0, 72)}
+                  .string {color:rgb(84, 216, 23)}
+                  .normalText {color: rgb(0, 0, 0)}
+                  .function {color:rgb(255, 170, 0)}
+                  .integer {color: darkgreen;}
+              </style>
+          </head>
+          <body>"
+            <> body <>
+          "</body>
+      </html>"
+  end
+
+  @doc """
+  function to add style to specific part with the corresponding class
+  """
+  def add_information(text, class) do
+    "<span class=#{class}>#{text}</span>"
+  end
+
+  @doc """
+  Write output back to file
+  """
+  def write_file(file_name, content) do
+    File.write!(file_name, content)
+  end
+
+  @doc """
+  Returns line in html format
+  function to add the HTML information to the line
+  """
+  def format_html(line) do
+    "<pre>#{line}</pre>"
+  end
+
+  @doc """
+  Key word identifier: recieves line identifies keywords and gives them a different color in output file
+  """
+  def keyword_identify(line) do
+    #Regex.replace(~r/(^((?!#).)*) *((and)|(exec)|(not)|(assert)|(finally)|(or)|(break)|(for)|(pass)|(class)|(from)|(print)|(continue)|(global)|(raise)|(def)|(if)|(return)|(del)|(import)|(try)|(elif)|(in)|(while)|(else)|(is)|(with)|(except)|(lambda)|(yield)) +/, line, &add_information(&1, "keyword") , global: false)
+    Regex.replace(~r/^((and)|(exec)|(not)|(assert)|(finally)|(or)|(break)|(for)|(pass)|(class)|(from)|(print)|(continue)|(global)|(raise)|(def)|(if)|(return)|(del)|(import)|(try)|(elif)|(in)|(while)|(else)|(is)|(with)|(except)|(lambda)|(yield))$/, line, &add_information(&1, "keyword") , global: false)
+  end
+
+  @doc """
+  Operator identifier (=, +, -, *): recieves a line identifies operators and gives them a different color in output file
+  """
+  def  operator_identify(line) do
+
+  end
+
+  @doc """
+  Variable identifier: recieves a line identifies variables and gives them a different color in output file
+  """
+  def variable_identify(line) do
+
+  end
+
 end
